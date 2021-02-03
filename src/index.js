4c3144efed8a9be64c60fc5be18087e027c37369
Kutaev Gennady Igorevich <gennady.kutaev@truetech.by>
2021-02-03 15:30:09 +0300

    feat: test passing

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (matrix === undefined || matrix.length == 0) return []
+  return matrix.reduce((acc, item, index)=>{
+    index % 2 == 0 ? acc.push(...item) : acc.push(...item.reverse())    
+    return acc
+  }, [])
 }
